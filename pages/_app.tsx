0b05d78c8e93cc9d02c6fabce2d6f5470c436a36
vadhe <aldyvadhe@gmail.com>
2021-05-04 19:22:23 +0800

    add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import 'modern-normalize'
+import theme from 'theme/theme'
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -11,7 +13,9 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Next.js TypeScript Quickstart</title>
       </Head>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </>
   )
 }
